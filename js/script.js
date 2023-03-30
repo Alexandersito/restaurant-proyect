91ae6394ec1afc1a5f891d14b7e0b20d5a124f56
Alexandersito <018101396i@uandina.edu.pe>
2023-03-30 13:46:14 -0500

    seccion header terminada

--- js/script.js
+++ js/script.js
@@ -2,13 +2,35 @@ var openIcon = document.getElementById('open-icon');
 var closeIcon = document.getElementById('close-icon');
 
 openIcon.addEventListener('click', function () {
+    $('#menu-celular').slideToggle(300);
+    document.getElementById('header').style.backgroundColor = '#121618';
     openIcon.style.display = 'none';
     closeIcon.style.display = 'block';
-    closeIcon.classList.add('fade-in');
+    closeIcon.classList.add('rotacion');
 });
 
 closeIcon.addEventListener('click', function () {
+    $('#menu-celular').slideToggle(300);
     closeIcon.style.display = 'none';
     openIcon.style.display = 'block';
-    openIcon.classList.add('fade-in');
+    openIcon.classList.add('rotacion');
+
+    setTimeout(function () {
+        document.getElementById('header').style.backgroundColor = 'transparent';
+    }, 300);
+
 });
+
+//========================================
+//MENU PARA CELULARES
+//========================================
+$(document).ready(function () {
+    $('.ri-arrow-right-s-line').click(function () {
+        $(this).closest('li').find('.submenu').slideToggle(300);
+        $(this).toggleClass('rotated');
+    });
+});
+
+
+
+
